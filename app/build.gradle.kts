0f1c4be2d396ed83a7b0fd5cd5bcd166e06adc42
tuanbmt753 <xuantuanbmt753@gmail.com>
2026-05-30 23:32:01 +0700

    setting run android 9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.numberfindinggame"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.numberfindinggame"
