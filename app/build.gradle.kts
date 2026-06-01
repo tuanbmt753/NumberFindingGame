e9cfe0bb2b57b0cf18f4be05db65fef577ba950f
tuanbmt753 <xuantuanbmt753@gmail.com>
2026-06-01 22:34:25 +0700

    thêm firebase để lưu dữ liệu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -32,11 +33,17 @@ android {
 }
 
 dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
+
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-database")
 
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/numberfindinggame/MainActivity.java
+++ app/src/main/java/com/example/numberfindinggame/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.numberfindinggame;
 
 import android.os.Bundle;
 
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -20,5 +26,31 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Test Firebase Realtime Database
+        FirebaseDatabase firebaseDatabase =
+                FirebaseDatabase.getInstance(
+                        "https://numberfindinggame-default-rtdb.asia-southeast1.firebasedatabase.app"
+                );
+
+        DatabaseReference database = firebaseDatabase.getReference();
+
+        database.child("Test")
+                .child("Message")
+                .setValue("Xin chao Firebase")
+                .addOnSuccessListener(unused -> {
+                    Toast.makeText(this,
+                            "Ghi dữ liệu thành công",
+                            Toast.LENGTH_SHORT).show();
+
+                    Log.d("Firebase", "Ghi thành công");
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(this,
+                            "Lỗi: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+
+                    Log.e("Firebase", e.getMessage());
+                });
     }
 }
