14b1cb098db6082d0abc744e22e6233386c79e4a
tuanbmt753 <xuantuanbmt753@gmail.com>
2026-06-03 22:21:05 +0700

    giao diện đăng ký

--- app/src/main/java/com/example/numberfindinggame/activity/auth/DangKyActivity.java
+++ app/src/main/java/com/example/numberfindinggame/activity/auth/DangKyActivity.java
@@ -1,6 +1,10 @@
 package com.example.numberfindinggame.activity.auth;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +16,47 @@ import com.example.numberfindinggame.R;
 
 public class DangKyActivity extends AppCompatActivity {
 
+     private TextView txtLogin ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dang_ky);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        getControl();
+        getView();
+
+    }
+
+    private void getView() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            txtLogin.setText(
+                    Html.fromHtml(
+                            "Đã có tài khoản? <font color='#143485'>Đăng nhập</font>",
+                            Html.FROM_HTML_MODE_LEGACY
+                    )
+            );
+        } else {
+            txtLogin.setText(
+                    Html.fromHtml(
+                            "Đã có tài khoản? <font color='#143485'>Đăng nhập</font>"
+                    )
+            );
+        }
+
+        txtLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(
+                    DangKyActivity.this,
+                    DangNhapActivity.class
+            );
+            startActivity(intent);
+            finish();
         });
+
+    }
+
+    private void getControl() {
+         txtLogin = findViewById(R.id.txtLogin);
     }
 }
